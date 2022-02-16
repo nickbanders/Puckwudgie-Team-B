dafed0f7b6f439272742362c2098ea348092a99d
jackarms <90922918+jackarms@users.noreply.github.com>
2022-02-15 20:37:50 -0600

    creation of initial view files and server code

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
