747ac2e132aa947d1abb566759df4ca5585ca87a
jackarms <90922918+jackarms@users.noreply.github.com>
2022-02-19 12:40:28 -0600

    creation of initial signup login layouts

---
+++ controllers/home-routes.js
@@ -0,0 +1,17 @@
+// file will contain all of the user-facing routes, such as homepage and login
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+
+router.get("/", (req, res) => {
+  res.render("homepage");
+  // we are rendering homepage.handlebars, the .handlebars is implied as we've already set handlebars as our template engine.
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+module.exports = router;
