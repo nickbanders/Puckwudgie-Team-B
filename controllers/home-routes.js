02fdbb2e175acca59eaa9009182bfec23df2c593
nickbanders <nickbanders@gmail.com>
2022-02-23 22:16:21 -0600

    fixed post and comment errors

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,7 +5,7 @@ const { Post, User, Comment } = require("../models");
 router.get("/", (req, res) => {
   console.log(req.session);
   Post.findAll({
-    attributes: ["id", "title", "post_text", "created_at"],
+    attributes: ["id", "title", "created_at"],
     order: [["created_at", "DESC"]],
     include: [
       {
