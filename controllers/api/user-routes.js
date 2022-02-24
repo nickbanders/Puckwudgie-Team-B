393f666cee912fefa6b8bb544c437d44c5367864
Benito <benitocavazosr@yahoo.com>
2022-02-23 19:38:35 -0600

    adding code to send email when a user register

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,41 @@
 const router = require("express").Router();
 const { User, Comment, Vote, Issue } = require("../../models");
+const nodemailer = require("nodemailer");
+
+async function main(email, username) {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+  let user_name = con
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'reinhold.mitchell45@ethereal.email', //testAccount.user,  generated ethereal user
+      pass: 'gnsa8V9jUXwaQtww77' //testAccount.pass,  generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"[Code] Blues " <noreply@codeblues.com>', // sender address
+    to: email, // list of receivers
+    subject: "Thanks for joining us @ [Code] Blue", // Subject line
+    text: "Thank you for joining in to our community", // plain text body
+    html: "<b>Thank you for joining in to our community</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+
 
 // get all users
 router.get("/", (req, res) => {
@@ -72,6 +108,9 @@ router.post("/", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+    main(req.body.email, req.body.username ).catch(console.error);
+
+
 });
 
 router.post("/login", (req, res) => {
